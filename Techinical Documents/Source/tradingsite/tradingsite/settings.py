5f76c28d03c26f4fb454bd0c2cc188d36082ddac
Stefan Court <stefanandrewcourt@gmail.com>
2023-10-09 19:16:11 +0100

    added logins and main apps

--- Techinical Documents/Source/tradingsite/tradingsite/settings.py
+++ Techinical Documents/Source/tradingsite/tradingsite/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Custom apps
+    'main.apps.MainConfig',
+    'logins.apps.LoginsConfig'
 ]
 
 MIDDLEWARE = [
